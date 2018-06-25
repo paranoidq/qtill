9c7386f0a22901ce9faafb3bbeb023938d0fddd4
paranoidq <paranoid.qian@gmail.com>
2018-06-25 21:51:32 +0800

    添加executor框架

---
+++ qtill-commons/src/main/java/me/qtill/commons/executor/ExecuteCallback.java
@@ -0,0 +1,19 @@
+package me.qtill.commons.executor;
+
+/**
+ * @author paranoidq
+ * @since 1.0.0
+ */
+public abstract class ExecuteCallback<T> {
+
+    public void onExecuteSuccess(T result) {
+        // TODO
+    }
+
+    public void onExecuteFailure(Throwable throwable) {
+        // TODO
+    }
+
+
+
+}
